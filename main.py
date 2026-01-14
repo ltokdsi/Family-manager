300f0dd4fc268f81e84e5a38040eebbe01ca1f2f
ltokdsi <ltokdsi@yandex.ru>
2026-01-14 14:03:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -46,120 +46,132 @@ def main():
     if not original_data:
         print('База данных сейчас пуста. Но вы можете использовать '
               'пункт 5, чтобы добавить задачи')
-    while True:
-        user_choice = show_menu()
-        if user_choice == '7':
-            print('Программа успешно завершена! До свидания!')
-            break
-
-        working_tasks = copying.copy_list(original_data)
-
-        if user_choice == '1':
-            print('Отчет 1. Список задач за прошедшие N дней')
-
-            while True:
-                today_input = input('Введите сегодняшнюю дату '
-                                    '(ДД.ММ.ГГГГ) или 0 '
-                                    'для отмены: ').strip()
-                if today_input == '0':
-                    current_date_number = None
-                    break
-                if utilities.validate_date(today_input):
-                    current_date_number = (
-                        utilities.date_to_number(today_input))
-                    break
-                else:
-                    print('Ошибка! Введите дату корректно в формате '
-                          '(ДД.ММ.ГГГГ). Помните: в месяце 1-12, '
-                          'а дней 1-31! Год от 1900 до 2100!')
-            if today_input == '0':
-                continue
-
-            number_of_days = None
-            while True:
-                number_of_days_string = input('Введите число дней N '
-                                   '(или 0 для отмены): ')
-                if number_of_days_string == '0':
-                    number_of_days = None
-                    break
-                if not number_of_days_string.isdigit():
-                    print('Введите целое положительное число!')
-                    continue
-                number_of_days = int(number_of_days_string)
-                if number_of_days < 0:
-                    print('Введите целое положительное число!')
-                    number_of_days = None
-                    continue
+    try:
+        while True:
+            user_choice = show_menu()
+            if user_choice == '7':
+                print('Программа успешно завершена! До свидания!')
                 break
 
-            if number_of_days is None:
-                continue
+            working_tasks = copying.copy_list(original_data)
 
-            filtered = []
-            for task in working_tasks:
-                task_date = (
-                    utilities.date_to_number(task['дата_выдачи']))
-                if current_date_number - task_date < number_of_days:
-                    if current_date_number >= task_date:
-                        filtered.append(task)
-            shell_sorting.shell_sort(filtered, logic.compare_report_1)
-            print_tasks(filtered, f"Задачи за последние "
-                                  f"{number_of_days} дней")
+            if user_choice == '1':
+                print('Отчет 1. Список задач за прошедшие N дней')
 
-        elif user_choice == '2':
-            print('Отчет 2. Проваленные задачи')
-            all_names = []
-            for task in original_data:
-                names = task['исполнитель'].lower()
-                if names not in all_names:
-                    all_names.append(names)
-            while True:
-                print(f"Доступные имена: {', '.join(all_names)}")
-                name = input('Введите имя члена семьи:'
-                             ' (0 для отмены) ').strip()
+                while True:
+                    today_input = input('Введите сегодняшнюю дату '
+                                        '(ДД.ММ.ГГГГ) или 0 '
+                                        'для отмены: ').strip()
+                    if today_input == '0':
+                        current_date_number = None
+                        break
+                    if utilities.validate_date(today_input):
+                        current_date_number = (
+                            utilities.date_to_number(today_input))
+                        break
+                    else:
+                        print('Ошибка! Введите дату корректно'
+                              ' в формате (ДД.ММ.ГГГГ). Помните:'
+                              ' в месяце 1-12, а дней 1-31!'
+                              ' Год от 1900 до 2100!')
+                if today_input == '0':
+                    continue
+
+                number_of_days = None
+                while True:
+                    number_of_days_string = (
+                        input('Введите число дней N'
+                              ' (или 0 для отмены): '))
+                    if number_of_days_string == '0':
+                        number_of_days = None
+                        break
+                    if not number_of_days_string.isdigit():
+                        print('Введите целое положительное число!')
+                        continue
+                    number_of_days = int(number_of_days_string)
+                    if number_of_days < 0:
+                        print('Введите целое положительное число!')
+                        number_of_days = None
+                        continue
+                    break
+
+                if number_of_days is None:
+                    continue
+
+                filtered = []
+                for task in working_tasks:
+                    task_date = (
+                        utilities.date_to_number(task['дата_выдачи']))
+                    if (current_date_number - task_date <
+                            number_of_days):
+                        if current_date_number >= task_date:
+                            filtered.append(task)
+                shell_sorting.shell_sort(filtered,
+                                         logic.compare_report_1)
+                print_tasks(filtered, f"Задачи за последние "
+                                      f"{number_of_days} дней")
+
+            elif user_choice == '2':
+                print('Отчет 2. Проваленные задачи')
+                all_names = []
+                for task in original_data:
+                    names = task['исполнитель'].lower()
+                    if names not in all_names:
+                        all_names.append(names)
+                while True:
+                    print(f"Доступные имена: {', '.join(all_names)}")
+                    name = input('Введите имя члена семьи:'
+                                 ' (0 для отмены) ').strip()
+                    if name == '0':
+                        print('Действие отменено')
+                        break
+                    if name.lower() in all_names:
+                        break
+                    else:
+                        print('Некорректное имя! Попробуйте снова')
                 if name == '0':
-                    print('Действие отменено')
-                    break
-                if name.lower() in all_names:
-                    break
-                else:
-                    print('Некорректное имя! Попробуйте снова')
-            if name == '0':
-                continue
+                    continue
 
-            filtered = []
-            for current_task in working_tasks:
-                if (current_task['исполнитель'].lower() ==
-                        name.lower()):
-                    if current_task['статус'] == 'провалена':
+                filtered = []
+                for current_task in working_tasks:
+                    if (current_task['исполнитель'].lower() ==
+                            name.lower()):
+                        if current_task['статус'] == 'провалена':
+                            filtered.append(current_task)
+                shell_sorting.shell_sort(filtered,
+                                         logic.compare_report_2)
+                print_tasks(filtered,
+                            f'Проваленные задачи исполнителя {name}')
+
+            elif user_choice == '3':
+                print('Отчет 3. Активные задачи (получена или '
+                      'в процессе)')
+                filtered = []
+                for current_task in working_tasks:
+                    current_status = current_task['статус']
+                    if (current_status == 'получена' or
+                            current_status == 'в процессе'):
                         filtered.append(current_task)
-            shell_sorting.shell_sort(filtered, logic.compare_report_2)
-            print_tasks(filtered,
-                        f'Проваленные задачи исполнителя {name}')
+                shell_sorting.shell_sort(filtered,
+                                         logic.compare_report_3)
+                print_tasks(filtered, 'Список активных задач')
 
-        elif user_choice == '3':
-            print('Отчет 3. Активные задачи (получена или '
-                  'в процессе)')
-            filtered = []
-            for current_task in working_tasks:
-                current_status = current_task['статус']
-                if (current_status == 'получена' or
-                        current_status == 'в процессе'):
-                    filtered.append(current_task)
-            shell_sorting.shell_sort(filtered,
-                                     logic.compare_report_3)
-            print_tasks(filtered, 'Список активных задач')
+            elif user_choice == '4':
+                print_tasks(original_data, 'Исхдный список '
+                                           'без сортировок')
+            elif user_choice == '5':
+                (data_editor.add_new_task
+                 ('family_tasks.txt', original_data))
+                original_data = utilities.read_tasks('family_tasks.txt')
 
-        elif user_choice == '4':
-            print_tasks(original_data, 'Исхдный список '
-                                       'без сортировок')
-        elif user_choice == '5':
-            (data_editor.add_new_task
-             ('family_tasks.txt', original_data))
-            original_data = utilities.read_tasks('family_tasks.txt')
+            elif user_choice == '6':
+                original_data = (data_editor.delete_task
+                                 ('family_tasks.txt', original_data))
+    except KeyboardInterrupt:
+        print('\nПрограмма принудительно остановлена')
+    finally:
+        print('Сессия завершена')
 
-        elif user_choice == '6':
-            original_data = data_editor.delete_task('family_tasks.txt'
-                                                    , original_data)
+
 if __name__ == '__main__':
     main()
